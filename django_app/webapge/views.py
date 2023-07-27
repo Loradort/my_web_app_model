e7d17dbf046077f73c2e95b492e78dcd520eb618
Loradort <stu6512732107@sskru.ac.th>
2023-07-27 10:07:41 +0700

    push works

--- django_app/webapge/views.py
+++ django_app/webapge/views.py
@@ -6,7 +6,7 @@ def home(request):
     # return HttpResponse("This is my django website")
 
 def about(request):
-    return HttpResponse("About me")
+    return render(request,'about.html')
 
 def contact(request):
-    return HttpResponse("Contact me")
+    return render(request,'contact.html')
