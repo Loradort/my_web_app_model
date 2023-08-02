d0ab91c30d830a5852185ef1840c0699c75fa867
Loradort <stu6512732107@sskru.ac.th>
2023-08-02 21:29:57 +0700

    upnew

--- README.md
+++ README.md
@@ -1,4 +1,12 @@
 # Django
 
+      python manage.py runserver
 
-  python manage.py runserver
+## การสร้าง admin 
+    python manage.py createsuperuser
+
+## การสร้าง app_model
+    python manage.py startapp
+
+## การเริ่ม project ของ app_model
+    python manage.py startproject
--- django_app/webapge/models.py
+++ django_app/webapge/models.py
@@ -1,25 +1,26 @@
 from django.db import models
 
-# Create your models here.
-prefix_chioces = (
+prefix_choices = (
     (1, "นาย"),
     (2, "นางสาว"),
     (3, "นาง"),
 )
 
-class Student(models.Model):
-    prefix_chioces = models.IntegerField(choices=prefix_chioces, default=1)
-    std_id = models.IntegerField()
-    name = models.CharField(max_length= 255)
-    last_name = models.CharField(max_length= 255)
-    phone = models.CharField(max_length= 15)
-    adress = models.TextField()
 
+class Student(models.Model):
+    std_id = models.IntegerField()
+    prefix = models.IntegerField(choices=prefix_choices, default=1)
+    name = models.CharField(max_length=255)
+    lastname = models.CharField(max_length=255)
+    phone = models.CharField(max_length=15)
+    address = models.TextField()
 
     class Meta:
         verbose_name = "Student"
         verbose_name_plural = "Student s"
 
     def __str__(self):
-        return self.name + " " + self.last_name
+        return self.name
 
+    def get_absolute_url(self):
+        return reversed("Student_detail", kwargs={"pk": self.pk})
