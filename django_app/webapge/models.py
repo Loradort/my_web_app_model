47647f8125c7d9aa2e2d5bf97ed219019692af10
Loradort <stu6512732107@sskru.ac.th>
2023-08-02 21:05:20 +0700

    up new

--- django_app/webapge/models.py
+++ django_app/webapge/models.py
@@ -1,3 +1,25 @@
 from django.db import models
 
 # Create your models here.
+prefix_chioces = (
+    (1, "นาย"),
+    (2, "นางสาว"),
+    (3, "นาง"),
+)
+
+class Student(models.Model):
+    prefix_chioces = models.IntegerField(choices=prefix_chioces, default=1)
+    std_id = models.IntegerField()
+    name = models.CharField(max_length= 255)
+    last_name = models.CharField(max_length= 255)
+    phone = models.CharField(max_length= 15)
+    adress = models.TextField()
+
+
+    class Meta:
+        verbose_name = "Student"
+        verbose_name_plural = "Student s"
+
+    def __str__(self):
+        return self.name + " " + self.last_name
+
